3d0f569f1020c1ec53a47ef0606f1be91dcee761
Lucas-723b <lucas.corp2015@gmail.com>
2024-10-14 23:56:20 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-Console.WriteLine("Smartphone Nokia:";
-Smartphone nokia = new Nokia ( numero :  "4002", modelo: " Modelo 2" , imei: "13");
+Console.WriteLine("Nokia:";
+Smartphone nokia = new Nokia ( numero :  "4002");
 nokia.Ligar();
-nokia,InstalarAplicativo("Whatsapp");
 
 Console.writeLine("\n");
 
-Console.WriteLine("Smartphone Iphone:");
-Smartphone iphone = new iphone( numero : "1046", modelo: "Modelo 5" , imei: "14");
+Console.WriteLine("Iphone:");
+Smartphone iphone = new iphone( numero : "1046");
 iphone.ReceberLigação();
-iphone.InstalarAplicativo("Telegram");
+
